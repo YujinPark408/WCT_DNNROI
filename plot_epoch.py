6cda7e85e81a2332f46482eca96d3988f9ced9b9
YujinPakr408 <yjp143680@cau.ac.kr>
2025-08-26 22:38:56 +0900

    Modify plot_epoch.py

--- plot_epoch.py
+++ plot_epoch.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
+# cp_dir = 'checkpoints_unet_mini/'
+cp_dir = 'checkpoints_hybridunet_mini/'
+
 inputs   = [
     # train-eval
     # 'model/unet-l23-cosmic500-e50-t1/loss.csv',
@@ -15,9 +18,12 @@ inputs   = [
     # 'model/unet-adam-l23-cosmic500-e50/loss.csv',
     # 'model/unet-adam-l23-cosmic500-e50/eval-loss.csv',
     
-    'test3-th10/loss.csv',
-    'test3-th10/eval-loss.csv',
+	#'test3-th10/loss.csv',
+	#'test3-th10/eval-loss.csv',
 
+	cp_dir + 'loss.csv',
+	cp_dir + 'eval-loss.csv',
+		
     # sample size
     # 'model/unet-l23-cosmic500-e50-t1/ep-87-85.csv',
     # 'sample-size-test-400/ep-87-85.csv',
@@ -25,6 +31,7 @@ inputs   = [
     # 'sample-size-test-200/ep-87-85.csv',
     # 'sample-size-test-50/ep-87-85.csv',
     ]
+
 labels = [
     # train-eval
     'Training',
@@ -38,12 +45,36 @@ labels = [
     # '50',
     ]
 
+
+def find_cross_epochs_simple(x, y1, y2):
+    crossings = []
+    for i in range(len(x) - 1):
+        if (y1[i] - y2[i]) * (y1[i+1] - y2[i+1]) < 0:
+            if ( i != 0):
+                crossings.append(x[i])
+    return crossings
+
+plt.figure(figsize=(12,8))
+
+
+
 for itag in range(len(inputs)) :
     data = np.genfromtxt(inputs[itag], delimiter=',')
     marker = '-o'
     if labels[itag].find('Val') > 0:
         marker = '-^'
     plt.plot(data[:,0], data[:,int(sys.argv[1])], marker,label=labels[itag])
+
+
+data1 = np.genfromtxt(inputs[0], delimiter=',')
+data2 = np.genfromtxt(inputs[1], delimiter=',')
+datax = data1[:,0]
+cross_epochs = find_cross_epochs_simple(datax, data1[:,int(sys.argv[1])], data2[:,int(sys.argv[1])])
+for epoch in cross_epochs:
+    print("Cross_epochs : " + str(epoch))
+    plt.axvline(x=epoch, color='red', linestyle='--', label=f'Cross @ {epoch}')
+
+
 
 fontsize = 26
 plt.legend(loc='best',fontsize=fontsize)
@@ -58,4 +89,5 @@ plt.yticks(fontsize=fontsize)
 # plt.ylabel("Pixel Purity", fontsize=fontsize)
 # plt.ylabel("ROI Efficiency", fontsize=fontsize)
 # plt.ylabel("ROI Purity", fontsize=fontsize)
-plt.show()
+# plt.show()
+plt.savefig(cp_dir + "loss_curve.png", dpi=300, bbox_inches='tight')
